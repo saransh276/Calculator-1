59d397bd42ef9330cc37f4660cc01a1d8be73112
saransh276 <saranshgautam276@gmail.com>
2025-09-16 10:58:42 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,118 @@
+from enum import nonmember
+from tkinter import *
+
+first_number=second_number=operator=None
+
+def get_digit(digit):
+    current= result_label['text']
+    new= current+str(digit)
+    result_label.configure(text=new)
+
+def clear():
+    result_label.configure(text='')
+
+def get_operator(op):
+    global first_number,operator
+
+    first_number= int(result_label['text'])
+    operator= op
+    result_label.config(text='')
+
+def get_result():
+    global first_number,second_number,operator
+
+    second_number= int(result_label['text'])
+
+    if operator == '+':
+        result_label.config(text=int(first_number + second_number))
+    elif operator == '-':
+        result_label.config(text=str(first_number - second_number))
+    elif operator == '*':
+        result_label.config(text=str(first_number * second_number))
+    else:
+        if second_number == 0:
+            result_label.config(text='Error')
+        else:
+            result_label.config(text=str(round(first_number / second_number,2)))
+
+
+root=Tk()
+root.title('Calculator')
+root.geometry('300x300')
+root.resizable(0,0)
+root.configure(bg='Grey')
+
+result_label=Label(root,text='',bg='Grey',fg='Black')
+result_label.grid(row=0,column=0,columnspan=5,pady=15)
+result_label.config(font=('Ariel',30,'bold'))
+
+btn7=Button(root,text='7',bg='Blue',fg='White',width=4, height=1,command=lambda :get_digit(7))
+btn7.grid(row=1,column=0)
+btn7.config(font=('Ariel',20,'bold'))
+
+btn8=Button(root,text='8',bg='Blue',fg='White',width=4, height=1,command=lambda:get_digit(8))
+btn8.grid(row=1,column=1)
+btn8.config(font=('Ariel',20,'bold'))
+
+btn9=Button(root,text='9',bg='Blue',fg='White',width=4, height=1,command=lambda :get_digit(9))
+btn9.grid(row=1,column=2)
+btn9.config(font=('Ariel',20,'bold'))
+
+btn_add=Button(root,text='+',bg='Blue',fg='White',width=4, height=1,command=lambda :get_operator('+'))
+btn_add.grid(row=2,column=0)
+btn_add.grid(row=1,column=3)
+btn_add.config(font=('Ariel',20,'bold'))
+
+btn4=Button(root,text='4',bg='Blue',fg='White',width=4, height=1,command=lambda :get_digit(4))
+btn4.grid(row=2,column=0)
+btn4.config(font=('Ariel',20,'bold'))
+
+btn5=Button(root,text='5',bg='Blue',fg='White',width=4, height=1,command=lambda :get_digit(5))
+btn5.grid(row=2,column=1)
+btn5.config(font=('Ariel',20,'bold'))
+
+btn6=Button(root,text='6',bg='Blue',fg='White',width=4, height=1,command=lambda :get_digit(6))
+btn6.grid(row=2,column=2)
+btn6.config(font=('Ariel',20,'bold'))
+
+btn_sub=Button(root,text='-',bg='Blue',fg='White',width=4, height=1,command=lambda :get_operator('-'))
+btn_sub.grid(row=2,column=3)
+btn_sub.config(font=('Ariel',20,'bold'))
+
+btn1=Button(root,text='1',bg='Blue',fg='White',width=4, height=1,command=lambda :get_digit(1))
+btn1.grid(row=3,column=0)
+btn1.grid(row=3,column=0)
+btn1.config(font=('Ariel',20,'bold'))
+
+btn2=Button(root,text='2',bg='Blue',fg='White',width=4, height=1,command=lambda :get_digit(2))
+btn2.grid(row=3,column=1)
+btn2.config(font=('Ariel',20,'bold'))
+
+btn3=Button(root,text='3',bg='Blue',fg='White',width=4, height=1,command=lambda :get_digit(3))
+btn3.grid(row=3,column=2)
+btn3.config(font=('Ariel',20,'bold'))
+
+btn_mul=Button(root,text='*',bg='Blue',fg='White',width=4, height=1,command=lambda :get_operator('*'))
+btn_mul.grid(row=4,column=0)
+btn_mul.grid(row=3,column=3)
+btn_mul.config(font=('Ariel',20,'bold'))
+
+btn_clr=Button(root,text='C',bg='Blue',fg='White',width=4, height=1,command=lambda :clear())
+btn_clr.grid(row=4,column=0)
+btn_clr.config(font=('Ariel',20,'bold'))
+
+btn0=Button(root,text='0',bg='Blue',fg='White',width=4, height=1,command=lambda :get_digit('0'))
+btn0.grid(row=4,column=1)
+btn0.config(font=('Ariel',20,'bold'))
+
+btn_eql=Button(root,text='=',bg='Blue',fg='White',width=4, height=1,command= get_result)
+btn_eql.grid(row=4,column=2)
+btn_eql.config(font=('Ariel',20,'bold'))
+
+btn_div=Button(root,text='/',bg='Blue',fg='White',width=4, height=1,command=lambda :get_operator('/'))
+btn_div.grid(row=5,column=0)
+btn_div.grid(row=4,column=3)
+btn_div.config(font=('Ariel',20,'bold'))
+
+
+root.mainloop()
